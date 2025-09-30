bbaba04f12bdee737ef87ae6553a4d40120d7a3b
mayankk1337 <ritiksingh2428@gmail.com>
2025-09-30 07:59:37 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const line = document.getElementById('line');
 let LineAngle = 0;
-window.rotateEnabled = true;
 
 function rotateLine() {
-	if (!window.rotateEnabled) return;
     LineAngle += 2; 
     line.style.transform = `rotate(${LineAngle}deg)`; 
 }
